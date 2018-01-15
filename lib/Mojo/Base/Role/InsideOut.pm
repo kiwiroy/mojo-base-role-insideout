c280faf0261770d189043ed0a5502d0d48657ca6
Roy Storey <kiwiroy@users.noreply.github.com>
2018-01-15 15:26:40 +1300

    POD update

--- lib/Mojo/Base/Role/InsideOut.pm
+++ lib/Mojo/Base/Role/InsideOut.pm
@@ -72,17 +72,40 @@ sub _count_objects {
 
 =head1 NAME
 
-Mojo::File::Role::InsideOut - The old inside out trick.
+Mojo::Base::Role::InsideOut - The old inside out trick.
 
 =head1 DESCRIPTION
 
-Using the once "popular", though not universally adopted,
-L<Inside Out Objects|perlobj#Inside-Out-objects>
+In order to add properties to L<non hash objects|perlobj#Non-Hash-Objects> such
+as L<Mojo::File>, L<Mojo::Collection> and L<Mojo::ByteStream>, this
+L<role|Role::Tiny> uses the once "experimental", though not universally adopted,
+L<Inside Out Objects|perlobj#Inside-Out-objects>.
+
+=head1 SYNOPSIS
+
+  package File::Reader;
+  use Mojo::Base 'Mojo::File';
+  use Role::Tiny::With;
+  with 'Mojo::Base::Role::InsideOut';
+
+  __PACKAGE__->attr(fh => sub { $_[0]->open() });
+
+=head1 PROPERTIES
+
+No properties are available from this module, but may be defined by calling
+classes using C<__PACKAGE__->attr>.
 
 =head1 METHODS
 
+The following methods are available, although as L<Role::Tiny> does not call
+import on roles, they should be called thus:
+
+  __PACKAGE__->method(@args);
+
 =head2 attr
 
-=head2 DESTROY
+This is synonymous with L<attr|Mojo::Base#attr> from L<Mojo::Base>, but uses a
+lexically scoped hash to record the properties in line with the
+L<inside out|perlobj#Inside-Out-objects> model.
 
 =cut
